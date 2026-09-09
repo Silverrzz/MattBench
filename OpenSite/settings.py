19cd0919de3e58231b6722514bce090dcc8e1f2a
Ellie <lewisfulterer@gmail.com>
2026-09-09 17:13:35 +0100

    Preserve HTTPS proxy and trusted origins for production

--- OpenSite/settings.py
+++ OpenSite/settings.py
@@ -23,6 +23,8 @@ SECRET_KEY = '@!zw2l8til1(0eb_nk+1w!(n78gqm&u)s)_v7#k6iseia@g9q0'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://chess.n9x.co', 'https://openbench.nocturn9x.space']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 HTML_MINIFY   = True
 APPEND_SLASH  = True
